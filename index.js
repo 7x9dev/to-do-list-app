dc9bea9e36d90f943945717e21f34cf07ecda1c9
7x9dev <103652226+7x9dev@users.noreply.github.com>
2022-10-05 15:43:34 +0300

    Added JS

---
+++ index.js
@@ -0,0 +1,18 @@
+const input = document.querySelector(".inputField");
+const btn = document.querySelector(".addBtn");
+const toDo = document.querySelector(".toDoInput");
+
+btn.addEventListener("click", () => {
+   const listItem = document.createElement("li");
+   listItem.innerText = input.value;
+   listItem.classList.add("listItemAdded");
+   toDo.appendChild(listItem);
+   input.value = "";
+
+   listItem.addEventListener("click", () => {
+      listItem.classList.add("listItemDone");
+   })
+   listItem.addEventListener("dblclick", () => {
+      toDo.removeChild(listItem);
+   });
+})
